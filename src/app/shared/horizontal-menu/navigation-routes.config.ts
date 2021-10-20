0643d4688f109ed09ad5847d5cb8901eb1fef936
alejj29 <38271051+alejj29@users.noreply.github.com>
2021-10-19 21:08:57 -0300

    agregar el items conporativos en el menu

--- src/app/shared/horizontal-menu/navigation-routes.config.ts
+++ src/app/shared/horizontal-menu/navigation-routes.config.ts
@@ -3,5 +3,8 @@ import { RouteInfo } from '../vertical-menu/vertical-menu.metadata';
 export const HROUTES: RouteInfo[] = [
   {
     path: '/dashboard', title: 'Dashboard', icon: 'ft-home', class: 'dropdown nav-item', isExternalLink: false, submenu: []
+  },
+  {
+    path: '/corporativos', title: 'Corporativos', icon: 'ft-home', class: 'dropdown nav-item', isExternalLink: false, submenu: []
   }
 ];
